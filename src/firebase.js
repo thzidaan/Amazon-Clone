4a5d6ea35e63defda7b1c6111f5524d83f32a2cf
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-02-17 21:20:29 -0600

    Firebase Added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD6-P_GJxpGO9DkCWdJvv5cq3kmL0S00cA",
+    authDomain: "challenge-fe8f5.firebaseapp.com",
+    projectId: "challenge-fe8f5",
+    storageBucket: "challenge-fe8f5.appspot.com",
+    messagingSenderId: "1052104980186",
+    appId: "1:1052104980186:web:d78b5f3557fd02d74a97e3",
+    measurementId: "G-KETD5EDF25"
+  };
